8919a25ddd8f89f1e28384954ec11800436262ad
Omkar_Shirsekar <iamomkar0007@gmail.com>
2026-06-20 18:40:59 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import express from "express"
+import express from "express";
 import { user } from "./db.js";
 
 const app = express();
-app.use(express.json()) 
-app.post("/signin",async(req,res )=>{
-    const name=req.body.name;
-    const email=req.body.email;
-    const age= req.body.age
+app.use(express.json());
+app.post("/signin", async (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const age = req.body.age;
 
-const userEntry= user.create({
+  const userEntry = user.create({
     name,
     email,
-    age
-})
-if(!userEntry){
-    res.send("Error While making entry")
-}
-    res.status(201).send({
-        name,
-        email,
-        age
-    })
-} )
-app.get("/",(req ,res)=>{
-    res.json({
-        msg:"Testing"
-    })
-})
+    age,
+  });
+  if (!userEntry) {
+    res.send("Error While making entry");
+  }
+  res.status(201).send({
+    name,
+    email,
+    age,
+  });
+});
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Testing",
+  });
+});
 
-app.listen(3000)
+app.listen(3000);
